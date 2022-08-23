e3b95406f96b44948244f48b278c79f7d9419914
prashanthdsouza <prashanthdsouza17@gmail.com>
2022-08-23 15:54:12 +0530

    140

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Cart />
+      <Header />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </Fragment>
   );
